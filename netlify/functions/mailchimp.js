d42db02817d93c82e344797ce090074666bd3919
Joey Sardella <josephsardella@gmail.com>
2023-11-25 08:12:33 -0800

    fixed mailchimp.js

--- netlify/functions/mailchimp.js
+++ netlify/functions/mailchimp.js
@@ -2,19 +2,55 @@ const axios = require('axios');
 
 exports.handler = async (event) => {
     const formData = JSON.parse(event.body);
-    const { email, firstName, lastName, tags } = formData;
+    const {
+        email,
+        firstName,
+        lastName,
+        tags,
+        'Email Address': emailAddress,
+        Subject,
+        Story,
+        'Organization Name': organizationName,
+        'Phone Number': phoneNumber,
+        'Business Address': businessAddress,
+        'Business Message': businessMessage,
+        'front-upload': frontUpload,
+        'back-upload': backUpload,
+        memberID,
+        groupNumber,
+        insurance
+    } = formData;
 
-    // Split tags string into an array if it's not empty
     const tagsArray = tags ? tags.split(',').map(tag => tag.trim()) : [];
 
+    let notes = '';
+    if (Story) {
+        notes += `Story: ${Story}\n`;
+    }
+    if (businessMessage) {
+        notes += `Business Message: ${businessMessage}\n`;
+    }
+    if (frontUpload) {
+        notes += `Front of Insurance Card: ${frontUpload}\n`;
+    }
+    if (backUpload) {
+        notes += `Back of Insurance Card: ${backUpload}\n`;
+    }
+
     const data = {
-        email_address: email,
+        email_address: email || emailAddress,
         status: 'subscribed',
         tags: tagsArray,
         merge_fields: {
             FNAME: firstName,
             LNAME: lastName,
-            // Add other merge fields here as needed
+            PHONE: phoneNumber,
+            ADDRESS: businessAddress || insurance,
+            MEMBERID: memberID,
+            GROUPNUM: groupNumber,
+            ORGNAME: organizationName,
+            SUBJECT: Subject,
+            NOTES: notes
         },
     };
 
