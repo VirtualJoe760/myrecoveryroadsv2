4de71a9536424b4a9fb2d4fa054e924b7fe1bed0
Joey Sardella <josephsardella@gmail.com>
2023-11-26 15:03:38 -0800

    added journey functionality

---
+++ netlify/functions/patientJourney.js
@@ -0,0 +1,30 @@
+const client = require("@mailchimp/mailchimp_marketing");
+
+client.setConfig({
+  apiKey: process.env.MAILCHIMP_API_KEY,
+  server: "us21",
+});
+
+const journeyId - 3120;
+
+const getJourneyStepId = async (journeyId) => {
+  try {
+    const response = await client.customerJourneys.get(journeyId);
+    // Assuming you want the first step or a specific step
+    const stepId = response.steps[0].id; // or use some condition to find the specific step
+    return stepId;
+  } catch (error) {
+    console.error('Error fetching journey details:', error);
+    throw error;
+  }
+};
+
+
+const run = async () => {
+  const response = await client.customerJourneys.trigger(journeyId, stepId, {
+    email_address: emailAddress,
+  });
+  console.log(response);
+};
+
+run();
