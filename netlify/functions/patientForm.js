45f0c8e87669ab57c5a7b0542ae570f0f0d1627e
Joey Sardella <josephsardella@gmail.com>
2023-11-28 00:50:36 -0800

    working with webhooks

--- netlify/functions/patientForm.js
+++ netlify/functions/patientForm.js
@@ -1,6 +1,5 @@
 const axios = require('axios');
 const querystring = require('querystring');
-const nodeMailer = require('nodemailer');
 
 exports.handler = async (event) => {
     if (event.httpMethod !== 'POST') {
@@ -20,20 +19,6 @@ exports.handler = async (event) => {
         'Authorization': `Basic ${Buffer.from(`anystring:${process.env.MAILCHIMP_API_KEY}`).toString('base64')}`,
         'Content-Type': 'application/json'
     };
-
-    // Configure NodeMailer
-    const transporter = nodeMailer.createTransport({
-        host: "smtp-mail.outlook.com",
-        secureConnection: false,
-        port: 587,
-        auth: {
-            user: process.env.MAIL_ADDRESS,
-            pass: process.env.MAIL_PASS,
-        },
-        tls: {
-            ciphers: 'SSLv3'
-        }
-    });
 
     try {
         // Mailchimp API request to add contact
@@ -58,23 +43,7 @@ exports.handler = async (event) => {
         const journeyAPI = `https://us21.api.mailchimp.com/3.0/customer-journeys/journeys/${journeyId}/steps/25234/actions/trigger`;
         await axios.post(journeyAPI, { email_address: formData.emailAddress }, { headers: mailchimpHeaders });
 
-        const mailOptions = {
-            from: process.env.MAIL_ADDRESS,
-            to: process.env.MRR_MAIL,
-            subject: 'New Patient Form Submission',
-            html: `<h1>New Patient Form Submission</h1><hr />
-                     <p>First Name: ${formData.firstName}</p>
-                     <p>Last Name: ${formData.lastName}</p>
-                     <p>Email: ${formData.emailAddress}</p>
-                     <p>Phone: ${formData.phone}</p>
-                     <p>Insurance: ${formData.insurance}</p>
-                     <p>Member ID: ${formData.memberID}</p>
-                     <p>Group Number: ${formData.groupNumber}</p>`
-        };
-
-        // Send email
-        await transporter.sendMail(mailOptions);
-        return { statusCode: 200' };
+        return { statusCode: 200, body: 'Form processed successfully' };
     } catch (error) {
         console.error('Error:', error);
         return { statusCode: 500, body: `Error processing the request: ${error.message}` };
